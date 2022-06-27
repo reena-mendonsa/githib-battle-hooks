39eea618487f9cfb607b2516e4a591430d494666
reena-mendonsa <reenamendonsa@gmail.com>
2022-06-27 16:37:18 +0530

    Update Final.js

--- github-battle-hooks/src/Components/Final.js
+++ github-battle-hooks/src/Components/Final.js
@@ -36,95 +36,89 @@ function Final(props) {
   return (
     <main className="pb-12">
       <section className="w-1/2 mx-auto flex justify-between mt-16">
-        <div
-          className={
-            darkMode
-              ? "flex-35 bg-gray-700 text-black p-6 rounded-md mr-10"
-              : "flex-35 bg-gray-200 text-black p-6 rounded-md mr-10"
-          }
-        >
-          <h2 className="text-center winner">Winner</h2>
-          <div>
-            <img
-              src={winner.avatar_url}
-              alt={winner.login}
-              width="100"
-              className="w-full"
-            />
+       <div className="container flex">
+          <div
+            className={
+              darkMode
+                ? "flex-35 bg-gray-700 text-black p-6 rounded-md mr-10"
+                : "flex-35 bg-gray-200 text-black p-6 rounded-md mr-10"
+            }
+          >
+            <div className=" flex45 box">
+              <h2 className="text-center winner">Winner</h2>
+              <div>
+                <img
+                  src={winner.avatar_url}
+                  alt={winner.login}
+                  width="100"
+                  className="w-full"
+                />
+              </div>
+              <h5 className="text-center">
+                Score: {winner.public_repos * 20 + winner.followers}
+              </h5>
+              <h2 className="">{winner.login}</h2>
+              <div className="flex">
+                <i className="fas fa-user text-red-400"></i>
+                <h3 className=" wl-h3">{winner.name}</h3>
+              </div>
+
+              <div className="flex">
+                <i className="fas fa-users text-blue-400"></i>
+                <h3 className=" wl-h3">{winner.followers} Followers</h3>
+              </div>
+              <div className="flex">
+                <i className="fas fa-users text-green-400"></i>
+                <h3 className=" wl-h3">{winner.following} Following</h3>
+              </div>
+              <div className="flex">
+                <i className="fas fa-code text-red-500"></i>
+                <h3 className=" wl-h3">{winner.public_repos} Repositories</h3>
+              </div>
+            </div>
           </div>
-          <h5 className="text-center">
-            Score: {winner.public_repos * 20 + winner.followers}
-          </h5>
-          <h2 className="">{winner.login}</h2>
-          <div className="text-xl">
-            <i className="fas fa-user text-red-400"></i>
-            <h3 className="inline-block mx-3">{winner.name}</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-compass text-blue-400"></i>
-            <h3 className="inline-block mx-3">{winner.location}</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-users text-blue-400"></i>
-            <h3 className="inline-block mx-3">{winner.followers} followers</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-users text-green-400"></i>
-            <h3 className="inline-block mx-3">{winner.following} following</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-code text-red-500"></i>
-            <h3 className="inline-block mx-3">
-              {winner.public_repos} repositories
-            </h3>
+          <div
+            className={
+              darkMode
+                ? "flex-35 bg-gray-700 text-black p-6 rounded-md mr-10"
+                : "flex-35 bg-gray-200 text-black p-6 rounded-md mr-10"
+            }
+          >
+            <div className="flex45 box">
+              <h2 className="text-center loser">Loser</h2>
+              <div>
+                <img
+                  src={loser.avatar_url}
+                  alt={winner.login}
+                  width="100"
+                  className="w-full"
+                />
+              </div>
+              <h5 className="text-center font-bold ">
+                Score: {loser.public_repos * 20 + winner.followers}
+              </h5>
+              <h2 className=" font-bold ">{loser.login}</h2>
+              <div className="flex">
+                <i className="fas fa-users icons" aria-hidden="true"></i>
+                <h3 className="wl-h3">{loser.name}</h3>
+              </div>
+
+              <div className="flex">
+                <i className="fas fa-users icons" aria-hidden="true"></i>
+                <h3 className="wl-h3">{loser.followers} Followers</h3>
+              </div>
+              <div className="flex">
+                <i className="fas fa-users icons" aria-hidden="true"></i>
+                <h3 className="wl-h3">{loser.following} Following</h3>
+              </div>
+              <div className="flex">
+                <i className="fas fa-code icons"></i>
+                <h3 className="wl-h3">{loser.public_repos} Repositories</h3>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div
-          className={
-            darkMode
-              ? "flex-35 bg-gray-700 text-black p-6 rounded-md mr-10"
-              : "flex-35 bg-gray-200 text-black p-6 rounded-md mr-10"
-          }
-        >
-          <h2 className="text-center loser">Loser</h2>
-          <div>
-            <img
-              src={loser.avatar_url}
-              alt={winner.login}
-              width="100"
-              className="w-full"
-            />
-          </div>
-          <h5 className="text-center my-2 font-bold ">
-            Score: {loser.public_repos * 20 + winner.followers}
-          </h5>
-          <h2 className="text-red-300 font-bold text-xl text-center mb-3">
-            {loser.login}
-          </h2>
-          <div className="text-xl">
-            <i className="fas fa-user text-red-400"></i>
-            <h3 className="inline-block mx-3">{loser.name}</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-compass text-blue-400"></i>
-            <h3 className="inline-block mx-3">{loser.location}</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-users text-blue-400"></i>
-            <h3 className="inline-block mx-3">{loser.followers} followers</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-users text-green-400"></i>
-            <h3 className="inline-block mx-3">{loser.following} following</h3>
-          </div>
-          <div className="text-xl">
-            <i className="fas fa-code text-red-500"></i>
-            <h3 className="inline-block mx-3">
-              {loser.public_repos} repositories
-            </h3>
-          </div>
-        </div>
       </section>
 
       <div className="text-center my-6">
